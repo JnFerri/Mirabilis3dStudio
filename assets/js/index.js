a1d9f858dba4d67e8c0f3e7bcbea086726e030eb
Jean Ferri <jeanferrimh@gmail.com>
2022-06-27 17:23:54 -0300

    27/06/2022

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,11 @@ var load = document.getElementById("preloader")
 
 var conteudo = document.getElementById("conteudo")
 
+var video = document.getElementById("video")
+
 setTimeout(function loading() {
 
     load.innerHTML = ""
-
     conteudo.style.visibility = "visible"
-}, 1500)
+video.play()
+}, 6500)
