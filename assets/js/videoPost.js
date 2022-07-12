eccb4ba114e17dc912fe2f88528c343dd18e0791
Jean Ferri <jeanferrimh@gmail.com>
2022-07-11 22:07:40 -0300

    11/07/2022

--- assets/js/videoPost.js
+++ assets/js/videoPost.js
@@ -1,35 +1,7 @@
-let imagens = []
-let qntImagem = 15
-for(a=1; a < qntImagem; a++){
-    let frame = `assets/imagens/imagens/frame (${a}).jpg`
-    imagens.push(frame)
-}
-console.log(imagens)
-let n = 0
-let localPost = document.getElementById('imagemPortifolio')
-localPost.innerHTML = `<img src='${imagens[n]}'></img>`
-function seguinte(){
-    n++
-    if(n > (imagens.length -1)){
-        n = 0}
-    localPost.innerHTML = `<img src='${imagens[n]}'></img>`
-    
-    }
-    
-function anterior(){
-    
-    n--
-    if(n < 0){
-        n = imagens.length - 1}
-    localPost.innerHTML = `<img src='${imagens[n]}'></img>`
-    
-}
-    
-
 
 
 let videos = []
-let qntVideos = 7
+let qntVideos = 8
 for(a=1; a < qntVideos; a++){
     let video = `assets/imagens/videos/video (${a}).mp4`
     videos.push(video)
@@ -38,6 +10,7 @@ for(a=1; a < qntVideos; a++){
 let v = 0
 let localPostVideo = document.getElementById('videoPortifolio')
 localPostVideo.innerHTML = `<video src='${videos[v]}' controls></video>`
+
 function seguinteVideo(){
     v++
     localPostVideo.innerHTML =''
@@ -56,4 +29,3 @@ function anteriorVideo(){
     localPostVideo.innerHTML = `<video src='${videos[v]}' controls></img>`
     
 }
-
